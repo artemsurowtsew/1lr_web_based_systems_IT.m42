ac21a92fdcd6c0707e6f2554501b46a351fade36
Artem Surovtsev <kontershadow@gmail.com>
2024-09-15 22:24:48 +0300

    task7_v2

--- task7.php
+++ task7.php
@@ -1,55 +1,66 @@
-<?php  
-class CSV { 
+<?php  class CSV { 
 
-    private $_csv_file = null;
+private $_csv_file = "C:\\Users\\Artem\\Desktop\\php\\excell.csv";
 
-    /** 
-    * @param string $csv_file  = "C:\\Users\\Artem\\Desktop\\php\\excell.csv"
-    */
-    public function __construct($csv_file) {         
-        if (file_exists($csv_file)) {  
-            $this->_csv_file = $csv_file;  
-        } else { 
-            throw new Exception("File not found: " . $csv_file);  
-        }
-    } 
 
-    public function setCSV(Array $csv) { 
-        $handle = fopen($this->_csv_file, "a"); // Open the file for writing
-        foreach ($csv as $value) {              
-            fputcsv($handle, explode(";", $value), ";");  
-        }         
-        fclose($handle);  
-    } 
+/** 
+
+* @param string $csv_file  = "C:\\Users\\Artem\\Desktop\\php\\excell.csv"
+
+*/     public function __construct($csv_file) {         if (file_exists($csv_file)) {  
+
+       $this->_csv_file = $csv_file;  
+
+   }         else throw new Exception("File not found");  
+
+} 
+
+
+public function setCSV(Array $csv) { 
+
+   $handle = fopen($this->_csv_file, "a"); // Open the file for writing
+
+foreach ($csv as $value) {              
+
+fputcsv($handle, explode(";", $value), ";");  
+
+   }         fclose($handle);  
+
+} 
 
     public function getCSV() { 
-        $handle = fopen($this->_csv_file, "r"); // Open the file for reading
-        $array_line_full = array();          
-        while (($line = fgetcsv($handle, 0, ";")) !== FALSE) {  
-            $array_line_full[] = $line;  
-        }         
-        fclose($handle);          
-        return $array_line_full;  
-    } 
-}  
 
-try { 
-    // Використовуйте абсолютний або відносний шлях до CSV файлу
-    $csv = new CSV("excell.csv"); 
-    
-    $get_csv = $csv->getCSV();     
-    foreach ($get_csv as $value) {          
-        echo "Last name: " . $value[0] . "<br/>";         
-        echo "First name: " . $value[1] . "<br/>";         
-        echo "Position: " . $value[2] . "<br/>";         
-        echo "Salary: " . $value[3] . "<br/>";         
-        echo "--------<br/>"; 
-    } 
+   $handle = fopen($this->_csv_file, "r"); // Open the file for reading 
 
-    $arr = array("Ponomarenko;Ivan;;12000",); 
-    $csv->setCSV($arr); 
 
-} catch (Exception $e) {     
-    echo "Помилка: " . $e->getMessage(); 
+   $array_line_full = array();          while (($line = fgetcsv($handle, 0, ";")) !== FALSE) {  
+
+       $array_line_full[] = $line;  
+
+   }         fclose($handle);          return $array_line_full;  
+
 } 
-?>
+
+
+}  try { 
+
+$csv = new CSV("excell.csv");      $get_csv = $csv->getCSV();     foreach ($get_csv as $value) {          
+echo "Last name: " . $value[0] . "<br/>";         echo "First name: " . $value[1] . "<br/>";         
+echo " Position: " . $value[2] . "<br/>";         echo " Salary: " . $value[3] . "<br/>";         
+echo "--------<br/>"; 
+
+} 
+
+
+
+$arr = array("Ponomarenko;Ivan;;12000",); 
+
+$csv->setCSV($arr); 
+
+} 
+
+catch (Exception $e) {     echo "Помилка: " . $e->getMessage(); 
+
+} 
+
+?> 
